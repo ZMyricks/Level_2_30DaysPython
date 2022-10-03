3b745e6f47dd8a9a3a061408e3fdd3f68278075f
ZMyricks <105020592+ZMyricks@users.noreply.github.com>
2022-10-03 10:25:16 -0700

    YouTube / PyTube

--- day13_youTubeDownloader/v5_PyTube.py
+++ day13_youTubeDownloader/v5_PyTube.py
@@ -4,34 +4,34 @@ from pytube import YouTube
 myVideo = YouTube('https://www.youtube.com/watch?v=8jQekSeFW9o')
 
 # Title of the Video
-# print(f"Video Title: {myVideo.title}")
-# # URL of the Thumbnail of the video
-# print(f"Thumbnail Link: {myVideo.thumbnail_url}")
-# # ID of the Video
-# print(f"Video ID: {myVideo.video_id}")
-# # Description of the Video
-# print(f"Description: {myVideo.description}")
-# # Length of the Video in Seconds
-# print(f"Duration: {myVideo.length}")
-# # Total Views of the Video
-# print(f"Views: {myVideo.views}")
-# # Age Restricted Content
-# print(f"Age Restricted: {str(myVideo.age_restricted)}")
+print(f"Video Title: {myVideo.title}")
+# URL of the Thumbnail of the video
+print(f"Thumbnail Link: {myVideo.thumbnail_url}")
+# ID of the Video
+print(f"Video ID: {myVideo.video_id}")
+# Description of the Video
+print(f"Description: {myVideo.description}")
+# Length of the Video in Seconds
+print(f"Duration: {myVideo.length}")
+# Total Views of the Video
+print(f"Views: {myVideo.views}")
+# Age Restricted Content
+print(f"Age Restricted: {str(myVideo.age_restricted)}")
 
 # #  ===================================================
-# myVideoStreams = myVideo.streams
-# print(myVideoStreams)
+myVideoStreams = myVideo.streams
+print(myVideoStreams)
 
 
 # #  ==============================================
 # # Hd Stream is best quality video
-# myHDStream = YouTube('https://www.youtube.com/watch?v=8jQekSeFW9o').streams.first()
-# myAudioStream = YouTube('https://www.youtube.com/watch?v=8jQekSeFW9o').streams.last()
-# print(myHDStream, myAudioStream)
+myHDStream = YouTube('https://www.youtube.com/watch?v=8jQekSeFW9o').streams.first()
+myAudioStream = YouTube('https://www.youtube.com/watch?v=8jQekSeFW9o').streams.last()
+print(myHDStream, myAudioStream)
 
 # # ==================================================
 
 # # dwownload to a relative path
-# myHDStream.download("Videos/")
+myHDStream.download("Videos/")
 
 #  =============================================
